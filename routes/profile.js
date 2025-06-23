5266ad8c7a03d04e142253c76644f1299ece38bc
ifrahZulfiqar <ifrahzulfiqar2002@gmail.com>
2025-06-23 20:51:09 +0500

    edit profile backend created

--- routes/profile.js
+++ routes/profile.js
@@ -65,4 +65,82 @@ router.get("/provider/:firebaseUID", async (req, res) => {
   }
 });
 
+router.put("/consumer/:firebaseUID", async (req, res) => {
+  try {
+    const { firebaseUID } = req.params;
+    const { Email, Name, ContactNumber, Categories } = req.body;
+
+    const user = await User.findOne({ FirebaseUID: firebaseUID, RoleType: "Consumer" });
+
+    if (!user) return res.status(404).json({ message: "Consumer not found" });
+
+    // Update fields if provided
+    if (Email) user.Email = Email;
+    if (Name) user.Name = Name;
+    if (ContactNumber) user.ContactNumber = ContactNumber;
+    if (Categories) user.Categories = Categories;
+
+    await user.save();
+
+    res.json({
+      message: "Profile updated successfully",
+      profile: {
+        Name: user.Name,
+        Email: user.Email,
+        ContactNumber: user.ContactNumber,
+        Rating: user.Rating,
+        ProfilePicture: user.ProfilePicture,
+        Interests: user.Interests || [],
+      },
+    });
+  } catch (err) {
+    console.error("Consumer update route error:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// PUT /api/provider/:firebaseUID
+router.put("/provider/:firebaseUID", async (req, res) => {
+  try {
+    const { firebaseUID } = req.params;
+    const { Name, Email, ContactNumber, Location, CNIC, BusinessType, Services, Interests } = req.body;
+
+    const provider = await User.findOne({ FirebaseUID: firebaseUID, RoleType: "Provider" });
+
+    if (!provider) return res.status(404).json({ message: "Provider not found" });
+
+    // Update fields if provided
+    if (Email) provider.Email = Email;
+    if (Name) provider.Name = Name;
+    if (ContactNumber) provider.ContactNumber = ContactNumber;
+    if (Location) provider.Location = Location;
+    if (CNIC) provider.CNIC = CNIC;
+    if (BusinessType) provider.BusinessType = BusinessType;
+    if (Services) provider.Services = Services;
+    if (Interests) provider.Interests = Interests;
+
+    await provider.save();
+
+    res.json({
+      message: "Profile updated successfully",
+      profile: {
+        Name: provider.Name,
+        Email: provider.Email,
+        ContactNumber: provider.ContactNumber,
+        Rating: provider.Rating,
+        ProfilePicture: provider.ProfilePicture,
+        CNIC: provider.CNIC,
+        Location: provider.Location,
+        BusinessType: provider.BusinessType || "Individual",
+        Services: provider.Services || [],
+        Interests: provider.Interests || [],
+      },
+    });
+  } catch (err) {
+    console.error("Provider update route error:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+
 module.exports = router;
